a42088d0d63c0c6837bc3fa9e47f01e44323d3e9
Julian So <ch.julian.so@gmail.com>
2021-09-26 12:13:49 -0400

    added add task

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,15 @@ import PropTypes from "prop-types";
 import Button from "./Button";
 
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('Click')
+  }
+
   return (
     <div>
       <header className="header">
         <h1>{title}</h1>
-        <Button color="green" text="Add" />
+        <Button color="green" text="Add" onClick={onClick} />
       </header>
     </div>
   );
